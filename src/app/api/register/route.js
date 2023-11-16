08a80b35242d24aed7ce3ef55f21cb0fb4d3c77a
MichaelOffor1999 <111914796+MichaelOffor1999@users.noreply.github.com>
2023-11-16 11:09:32 +0000

    first commit

---
+++ src/app/api/register/route.js
@@ -0,0 +1,64 @@
+export async function GET(req, res) {
+
+  // Make a note we are on
+  // the api. This goes to the console.
+  console.log("in the api page")
+
+
+  // get the values
+  // that were sent across to us.
+  const { searchParams } = new URL(req.url)
+  const email = searchParams.get('email')
+  const pass = searchParams.get('pass')
+   // catch others
+  const address = searchParams.get('address')
+  const phoneNumber = searchParams.get('phoneNumber')
+  const dob = searchParams.get('dob')
+
+
+ 
+
+
+  console.log(email);
+  console.log(pass);
+  console.log(address);
+  console.log(phoneNumber)
+  console.log(dob)
+
+
+ 
+
+  // database call goes here
+
+// =================================================
+const { MongoClient } = require('mongodb');
+
+
+const url = 'mongodb://root:example@localhost:27017/';
+
+
+const client = new MongoClient(url);
+const dbName = 'app'; // database name
+await client.connect();
+console.log('Connected successfully to server');
+const db = client.db(dbName);
+const collection = db.collection('login'); // collection name
+
+
+
+
+const findResult = await collection.insertOne({"username":email, "pass": pass, "address": address, "phoneNumber": phoneNumber, "dob": dob});
+
+
+
+
+
+
+//==========================================================
+
+
+
+  // at the end of the process we need to send something back.
+  return Response.json({ "data":"" + valid + ""})
+}
+
---
+++ src/app/page.js
@@ -0,0 +1,168 @@
+'use client';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {ThemeProvider } from '@mui/material/styles';
+
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+
+export default function Page() {
+
+
+
+  /*
+  This function does the actual work
+  calling the fetch to get things from the database.
+  */ 
+  async function runDBCallAsync(url) {
+
+    // calling the api
+    const res = await fetch(url);
+    const data = await res.json();
+
+ 
+    if(data.data== "valid"){
+      console.log("login is valid!")
+
+      
+    } else {
+
+      console.log("not valid  ")
+    }
+  }
+
+
+  /*
+
+  When the button is clicked, this is the event that is fired.
+  The first thing we need to do is prevent the default refresh of the page.
+  */
+	const handleSubmit = (event) => {
+		
+		console.log("handling submit"); //This is for feedback, to show that handleSubmit was called
+
+
+    event.preventDefault(); //To prevent the browser from refreshing
+  
+		const data = new FormData(event.currentTarget);//Get all content from the form and store in data
+
+
+    let email = data.get('email') //Get email from the form
+		let pass = data.get('pass') //Get password from the form
+
+    console.log("Sent email:" + email)
+    console.log("Sent pass:" + pass)
+
+
+    runDBCallAsync(`http://localhost:3000/api/login?email=${email}&pass=${pass}`) //Function to send data to API page to talk to database
+
+
+
+
+  }; // end handler
+
+
+
+
+  
+  const theme = createTheme({
+    palette: {
+     
+      secondary: {
+        main: green[500],
+      },
+    },
+  });
+  
+
+
+
+  
+  return (
+    <ThemeProvider theme={theme}>
+    <Container component="main"  maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="pass"
+            label="Pass"
+            type="pass"
+            id="pass"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+
+
+
+
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+
+    </Container>
+
+    </ThemeProvider>
+
+  );
+}
